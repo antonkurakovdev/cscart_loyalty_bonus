83fa8bf6a9e121be28eb25c4d9ca20a467aa7166
antonkarton <96166976+antonkurakovdev@users.noreply.github.com>
2022-11-02 10:08:51 +0400

    2.1.1 Major changes
    - upgrader addon.xml to 3.0
    - added addon icon
    - added language variables
    - added copyright
    - added marketplace connector

--- app/addons/altteam_loyalty_bonus/init.php
+++ app/addons/altteam_loyalty_bonus/init.php
@@ -1,13 +1,13 @@
 <?php
+
 /*****************************************************************************
  * This is a commercial software, only users who have purchased a  valid
- * license and accepts the terms of the License Agreement can install and use  
+ * license and accepts the terms of the License Agreement can install and use
  * this program.
  *----------------------------------------------------------------------------
- * @copyright  LCC Alt-team: http://www.alt-team.com
- * @module     "Loyalty bonus" 
- * @version    4.x.x 
- * @license    http://www.alt-team.com/addons-license-agreement.html
+ * @copyright  LCC Alt-team: https://www.alt-team.com
+ * @module     "Loyalty bonus"
+ * @license    https://www.alt-team.com/addons-license-agreement.html
  ****************************************************************************/
 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
